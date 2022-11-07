7f93b96970fba0ddbfe7fd79b733bbf91bd03592
Hieu Pham <hieupm2096@gmail.com>
2022-11-08 01:49:40 +0700

    chore: prod s3 upload provider config, rds database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,10 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USER"),
-      password: env("DATABASE_PASSWORD"),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
   },
 });
