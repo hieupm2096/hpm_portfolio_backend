8088c43b7d92a142a2cad351ecea0f7d0a3ac8e6
Hieu Pham <hieupm2096@gmail.com>
2023-02-10 17:06:57 +0700

    chore: cloudinary file upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,27 @@
 module.exports = ({ env }) => ({
-   'transformer': {
-      enabled: true,
-      config: {
-        prefix: '/api/',
-        responseTransforms: {
-          removeAttributesKey: true,
-          removeDataKey: true,
-        }
-      }
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: "/api/",
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
     },
-  });
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
